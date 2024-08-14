f1c4e189da53c18f687a2c5439e32b5b293af4bb
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-08-14 19:51:44 +0300

    add grid template

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
-
-import './App.css';
-
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0qCreqkTZL0F0bF9kZctFE1XVFocO__70kw&s"
+                    alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className="content">
+                <div><img
+                    src="https://static.vecteezy.com/system/resources/previews/006/180/174/non_2x/panther-chameleon-on-branch-free-photo.jpg"
+                    alt="chameleon"/></div>
+                <div>
+                    ava + descript
+                </div>
+                <div>My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                    </div>
+                </div>
 
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default App;
