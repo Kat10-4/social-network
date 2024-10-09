07539fedfde263c992e45e673a8199191735b166
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-10-09 18:03:05 +0200

    add props for Post

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import s from './Post.module.css'
 
-const Post = () => {
+
+
+const Post = (props:any) => {
+
     return (
         <div className={s.item}>
             <img
                 src={'https://parade.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk0OTMxNTc0Njc1NzQzOTA2/avatar-3-evil-navi-james-cameron.jpg'}
                 alt={'Avatar'}/>
-            Post 1
+            {props.message}
+            <div>
+                <span>{props.likeCount} like </span>
+                <span> dislike</span>
+            </div>
         </div>
     )
 };
