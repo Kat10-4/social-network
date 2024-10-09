5a6575d8afb83e0cc32af1fd9b6a4ff9988dac3b
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-10-09 20:40:20 +0200

    added NavLinks

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}><a href={'/profile'}>Profile</a></div>
-            <div className={s.item}><a href={'/messages'}>Messages</a></div>
-            <div className={`${s.item} ${s.active}`}><a>News</a></div>
-            <div className={s.item}><a>Music</a></div>
-            <div className={s.item}><a>Settings</a></div>
+            <div className={s.item}>
+                <NavLink to={'/profile'}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={'/messages'}>Messages</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to={''}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={''}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to={''}>Settings</NavLink>
+            </div>
         </nav>
     );
 };
