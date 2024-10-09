a7e6ba1acfe759694b96dafad99c8a3802a12d9d
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-10-09 13:18:17 +0200

    add folders for each component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className={s.nav}>
             <div className={s.item}><a>Profile</a></div>
             <div className={s.item}><a>Messages</a></div>
-            <div className={s.item}><a>News</a></div>
+            <div className={`${s.item} ${s.active}`}><a>News</a></div>
             <div className={s.item}><a>Music</a></div>
             <div className={s.item}><a>Settings</a></div>
         </nav>
